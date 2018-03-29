0850126358f1cba317c0285f71c6377a2e7eb4d4
Jaco Pretorius <jaco@warejacob.co.za>
2018-03-29 15:35:24 +0200

    Create unbloat17.sh

---
+++ src/unbloat17.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+echo "Making Ubuntu great again..."
+sudo apt-get remove --purge libreoffice*
+sudo apt-get remove fonts-opensymbol libreoffice libreoffice-\* openoffice.org-dtd-officedocument1.0 python\*-uno uno-libs3-\* ure ure-dbg
+sudo apt-get remove libreoffice-core
+sudo apt purge ubuntu-web-launchers
+sudo apt-get remove banshee
+sudo apt-get remove rhythmbox
+sudo apt-get remove --purge libreoffice-core
+sudo apt-get purge gnome-games-common gbrainy && sudo apt-get autoremove
+sudo apt-get purge aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy
+sudo apt remove aisleriot gnome-mahjongg gnome-mines gnome-sudoku 
+sudo apt-get purge thunderbird*
+sudo apt-get purge transmission-gtk
+sudo apt-get purge transmission-cli transmission-common transmission-daemon
+sudo apt-get purge transmission*
+sudo apt-get remove cheese
+sudo apt-get purge shotwell*
+sudo apt-get autoremove && sudo apt-get clean all && sudo apt-get autoclean all
+#lets get the VM updated shall we
+sudo apt-get update
+sudo apt-get upgrade -y
+#all done
+echo "OK!"
