15884789a639e22c7153bfd99026fa5c4e9c0919
Jaco Pretorius <jaco@warejacob.co.za>
2018-03-29 17:30:58 +0200

    Create unbloat17_vb_tools_32.sh

---
+++ src/unbloat17_vb_tools_32.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+echo "Making Ubuntu great again..."
+#this prepares the VM for Virtualbox Tools installation
+sudo apt install linux-headers-$(uname -r) build-essential dkms -y
+#and clean all the crapware we dont want
+sudo apt-get remove --purge libreoffice* -y
+sudo apt-get remove fonts-opensymbol libreoffice libreoffice-\* openoffice.org-dtd-officedocument1.0 python\*-uno uno-libs3-\* ure ure-dbg -y
+sudo apt-get remove libreoffice-core -y
+sudo apt purge ubuntu-web-launchers -y
+sudo apt-get remove banshee -y
+sudo apt-get remove rhythmbox -y
+sudo apt-get remove --purge libreoffice-core -y
+sudo apt-get purge gnome-games-common -y
+sudo apt-get purge aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy -y
+sudo apt remove aisleriot gnome-mahjongg gnome-mines gnome-sudoku -y
+sudo apt-get purge thunderbird* -y
+sudo apt-get purge transmission-gtk -y
+sudo apt-get purge transmission-cli transmission-common transmission-daemon -y
+sudo apt-get purge transmission* -y
+sudo apt-get remove cheese -y
+sudo apt-get purge shotwell* -y
+sudo apt-get remove simple-scan -y
+sudo apt-get autoremove && sudo apt-get clean all && sudo apt-get autoclean all -y
+#lets get the VM updated shall we
+sudo apt-get update
+sudo apt-get upgrade -y
+#install VS Code - our text editor of choice
+sudo mkdir tmp
+cd tmp
+sudo wget -O VSCode.deb https://go.microsoft.com/fwlink/?LinkID=760680
+sudo dpkg -i VSCode.deb
+sudo apt-get install -f -y
+cd ..
+sudo rm -rf tmp
+#all done
+echo "Cleaned all OK and installed VS Code!"
+echo "now insert the VirtualBox GuestAdditions CD Image from the Devices menu and reboot."
